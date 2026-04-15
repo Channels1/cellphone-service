ed1b94dddb2dc5eb9ba5b21bb94e5044fb7011e9
jihaddrake <ringod40@gmail.com>
2026-04-15 14:04:26 -0400

    Updated CellPhone to have constructor methods and return methods.

---
+++ src/main/java/com/pluralsight/CellPhoneApplication.java
@@ -0,0 +1,28 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class CellPhoneApplication {
+    public static void main(String[] args) {
+        Scanner uKey = new Scanner(System.in);
+
+        System.out.println("What is your serial number? ");
+        int serialNumber = uKey.nextInt();
+
+        System.out.println("What is your phone's model? ");
+        String model = uKey.nextLine().trim();
+
+        System.out.println("What is your phone's model? ");
+        String model = uKey.nextLine().trim();
+
+        System.out.println("What is your phone's model? ");
+        String model = uKey.nextLine().trim();
+
+        System.out.println("Who is the owner of the phone? ");
+        String model = uKey.nextLine().trim();
+
+
+
+
+    }
+}
